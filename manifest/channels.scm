9a812631063c4e5d9c634dbce8962e6887d17daa
aletheios42 <alejandropintosdev@gmail.com>
2026-07-17 17:24:58 +0200

    fix: Canales de guix

--- manifest/channels.scm
+++ manifest/channels.scm
@@ -3,7 +3,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (introduction
          (make-channel-introduction
-          "897c1ad470db72c7b92a04930d8d11b49af3a51d"
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
           (openpgp-fingerprint
-           "3CE4 6455 8A84 FDC6 9DB4  0CFB 090B 1199 3D9A EBB5"))))
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
        %default-channels)
